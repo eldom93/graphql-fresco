bc6e40fe0000ee330dc729a4985286c6948c85d0
eldom93 <55229671+eldom93@users.noreply.github.com>
2020-02-15 13:21:03 -0600

    gql fresco

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema.js');
+const app = express();
+
+//entry point for any client that wants to interact with graphql on server
+app.use('/graphql', expressGraphQL({
+    schema:schema,
+    graphiql:true
+}));
+app.listen(4000, () => {
+    console.log('Server is running on port 4000!');
+});
