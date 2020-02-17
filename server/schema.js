37347a73fc4524bf06ce407406ce4da5297cf4e8
eldom93 <60298751+GaboEnterprise@users.noreply.github.com>
2020-02-17 00:53:50 -0600

    add(frontend):client

---
+++ server/schema.js
@@ -0,0 +1,15 @@
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema,
+    GraphQLList,
+    GraphQLNonNull
+} = require('graphql');
+
+// Schema Type
+module.exports = new GraphQLSchema({
+
+});
+
+
